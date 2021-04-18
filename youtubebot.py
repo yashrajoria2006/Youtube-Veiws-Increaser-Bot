a0700411a69d234ace976963efcbf7a0f8b3a77f
yashrajoria2006 <82503342+yashrajoria2006@users.noreply.github.com>
2021-04-18 17:14:55 +0530

    Update youtubebot.py

--- youtubebot.py
+++ youtubebot.py
@@ -1,10 +1,16 @@
+#pip install selenium
+
 from selenium import webdriver
 from time import sleep
+
+# For This You Have To Download Chrome Driver
 
 driver1 = webdriver.Chrome(executable_path="C:\\Users\\Nagendra\\Downloads\\Chromedriver\\chromedriver")
 driver2 = webdriver.Chrome(executable_path="C:\\Users\\Nagendra\\Downloads\\Chromedriver\\chromedriver")
 driver3 = webdriver.Chrome(executable_path="C:\\Users\\Nagendra\\Downloads\\Chromedriver\\chromedriver")
 driver4 = webdriver.Chrome(executable_path="C:\\Users\\Nagendra\\Downloads\\Chromedriver\\chromedriver")
+
+# Browser
 
 driver1.get("https://youtu.be/P-rk9FdxdTY")
 driver2.get("https://youtu.be/P-rk9FdxdTY")
