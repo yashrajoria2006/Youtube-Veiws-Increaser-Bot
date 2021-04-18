9fce11c3e91300a521adf716eada002b16e1b676
yashrajoria2006 <82503342+yashrajoria2006@users.noreply.github.com>
2021-04-18 17:13:10 +0530

    Add files via upload

---
+++ youtubebot.py
@@ -0,0 +1,19 @@
+from selenium import webdriver
+from time import sleep
+
+driver1 = webdriver.Chrome(executable_path="C:\\Users\\Nagendra\\Downloads\\Chromedriver\\chromedriver")
+driver2 = webdriver.Chrome(executable_path="C:\\Users\\Nagendra\\Downloads\\Chromedriver\\chromedriver")
+driver3 = webdriver.Chrome(executable_path="C:\\Users\\Nagendra\\Downloads\\Chromedriver\\chromedriver")
+driver4 = webdriver.Chrome(executable_path="C:\\Users\\Nagendra\\Downloads\\Chromedriver\\chromedriver")
+
+driver1.get("https://youtu.be/P-rk9FdxdTY")
+driver2.get("https://youtu.be/P-rk9FdxdTY")
+driver3.get("https://youtu.be/P-rk9FdxdTY")
+driver4.get("https://youtu.be/P-rk9FdxdTY")
+
+while True:
+    sleep(20)
+    driver1.refresh()
+    driver2.refresh()
+    driver3.refresh()
+    driver4.refresh()
